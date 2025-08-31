c0ac1732263004e1a582eba6c09a831d370778c1
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-31 23:41:00 +0530

    changes

---
+++ Server/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config' ;
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 4000;
+ 
+
