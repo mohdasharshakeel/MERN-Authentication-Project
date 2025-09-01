407cc985f74d6a300ce8433bf61303a0997054a5
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-01 12:37:30 +0530

    schema

---
+++ Server/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+
+    },
+    name: {
+        type: String,
+        required: true,
+        unique : true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    verifyOTP : {
+        type : String,
+        default : ''
+    },
+    verifyOtpExpireAt : {
+        type : Number,
+        default : 0
+    },
+    isVerifiedAccount :{
+        type : Boolean,
+        default : false
+
+    },
+    resetOtp :{
+        type: String,
+        default: ""
+    },
+    resetOtpExpireAt :{
+        type: Number,
+        default: 0
+    }
+
+
+})
