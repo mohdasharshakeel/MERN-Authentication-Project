41a59df2f3c8bec40a740b68563eb6e4fac12603
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-01 12:05:47 +0530

    server.js

--- Server/server.js
+++ Server/server.js
@@ -5,5 +5,17 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 4000;
- 
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credientials:true}))
+
+app.get('/',(req, res)=>{
+ res.send("API is working fine ")
+})
+
+
+app.listen(port, ()=>{
+    console.log("server is running on " + port )
+})
 
