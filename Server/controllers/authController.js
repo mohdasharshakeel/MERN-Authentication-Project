ee9b04dcf9a108ad65b28284145a403833604b22
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-02 10:22:42 +0530

    login controller

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -2,29 +2,70 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "All fields are required" });
+  }
+  try {
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.json({ success: true, message: "Register successful" });
+  } catch (error) {
+    res, json({ success: false, message: error.message });
+  }
+};
 
-export const register = async (req, res)=>{
-    const {name, email, password} = req.body;
-    if(!name || !email || !password){
-        return res.json({success : false , message : "All fields are required"});
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "email or password is incorrect",
+      });
     }
-    try{
-        const existingUser = await userModel.findOne({ email })
-        if (existingUser) {
-            return res.json({success : false , message : "User already exists"});
-         }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new userModel({ name, email, password: hashedPassword });
-        await user.save()
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            maxAge : 7 * 24 * 60 * 60 * 1000
-        })
-    }catch(error){ 
-      res,json({success : false , message : error.message});
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: "email or password is incorrect",
+      });
     }
- 
-}
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true, message: "Login successful" });
+
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
