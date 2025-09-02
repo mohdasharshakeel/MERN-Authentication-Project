3615785c2fcc3f12e2671635e520fdbd1a036d87
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-02 10:05:51 +0530

    token added

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -16,8 +16,14 @@ export const register = async (req, res)=>{
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new userModel({ name, email, password: hashedPassword });
         await user.save()
-
-    }catch(error){
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge : 7 * 24 * 60 * 60 * 1000
+        })
+    }catch(error){ 
       res,json({success : false , message : error.message});
     }
  
