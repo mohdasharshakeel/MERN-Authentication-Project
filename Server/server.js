d9ed3c98c390b94bdd0a98276ae01cb9ddafb355
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-03 19:25:32 +0530

    server.js route added

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config' ;
 import cookieParser from "cookie-parser";
+
+
 import connectDB from "./config/mongodb.js";
+import authRuter from "./routes/authRoute.js"
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,6 +15,10 @@ connectDB()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credientials:true}))
+
+//! API endpoints
+app.use('/api/auth', authRuter )
+
 
 app.get('/',(req, res)=>{
  res.send("API is working fine ")
