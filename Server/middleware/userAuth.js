724ca6166483477475257b8d62dafad0277f5805
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-15 16:14:13 +0530

    token decode

--- Server/middleware/userAuth.js
+++ Server/middleware/userAuth.js
@@ -3,5 +3,25 @@ import jwt from "jsonwebtoken"
 const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
 
-    if(!token)
+    if (!token) {
+        return res.json({success:false, message : "Not authorization login again"})
+    }
+
+    try {
+        const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        if (tokenDecoded.id) {
+            req.body.userId = tokenDecoded.id;
+
+        } else {
+             return res.json({success:false, message:"Not Authorized Login again"})
+        }
+        
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+
+        })
+    }
 }
